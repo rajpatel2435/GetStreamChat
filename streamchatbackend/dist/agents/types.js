12aabc670fe8be144026be0dfbe1ce3b2b72e4ac
rajpatel2435 <rpatel@pacificdev.com>
2025-08-17 16:28:52 -0400

    feat: frontend part

--- streamchatbackend/dist/agents/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AgentPlatform = void 0;
-var AgentPlatform;
-(function (AgentPlatform) {
-    AgentPlatform["OPENAI"] = "openai";
-    AgentPlatform["WRITING_ASSISTANT"] = "writing_assistant";
-})(AgentPlatform || (exports.AgentPlatform = AgentPlatform = {}));
